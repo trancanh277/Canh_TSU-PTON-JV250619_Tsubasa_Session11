f344851bb8ec03dea52a57776c235fd32cafc0cb
trancanh277 <tranminhcanh.tp@gmail.com>
2025-07-23 21:54:29 +0900

    upload session 11 bai tap 1

---
+++ baitap01.js
@@ -0,0 +1,3 @@
+let year = +prompt("Nhập vào năm muốn kiểm tra :");
+if( year % 4 === 0 && year % 100 !== 0) {alert(`Năm bạn nhập vào là năm nhuận`)}
+else {alert (`Năm bạn nhập vào không phải là năm nhuận`)}
